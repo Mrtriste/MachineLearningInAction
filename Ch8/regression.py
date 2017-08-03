0abd65f9adc7d4ea016b3f1d76dcd5edc36e277d
Jincheng Wang <jinchengwone@gmail.com>
2017-08-03 14:58:04 +0800

    update regression.py

--- Ch8/regression.py
+++ Ch8/regression.py
@@ -2,6 +2,9 @@
 
 from numpy import *
 import matplotlib.pyplot as plt
+from time import sleep
+import json
+import urllib2
 
 def loadDataSet(fileName):
 	numFeat = len(open(fileName).readline().split('\t')) - 1
@@ -85,9 +88,88 @@ def lwlrMain():
 	ax.scatter(xMat[:,1].flatten().A[0], mat(yArr).T.flatten().A[0] , s=2,c='red')
 	plt.show()
 
-####################################8.4 Shrinking coefficients to understand our data
-#TOD0
+def rssError(yArr,yHatArr):
+	return ((yArr-yHatArr)**2).sum()
+
+#################################### ridgeRegres
+def ridgeRegres(xMat,yMat,lam=0.2):
+	xTx = xMat.T*xMat
+	denom = xTx + eye(shape(xMat)[1])*lam
+	if linalg.det(denom) == 0.0:
+		print "This matrix is singular, cannot do inverse"
+		return
+	ws = denom.I * (xMat.T*yMat)
+	return ws
+
+def ridgeTest(xArr,yArr):
+	xMat = mat(xArr); yMat=mat(yArr).T
+	yMean = mean(yMat,0)
+	yMat = yMat - yMean
+	xMeans = mean(xMat,0) # 均值，对每一列求均值
+	xVar = var(xMat,0) # 方差
+	xMat = (xMat - xMeans)/xVar # 随机变量标准化
+	numTestPts = 30
+	wMat = zeros((numTestPts,shape(xMat)[1]))
+	for i in range(numTestPts):
+		ws = ridgeRegres(xMat,yMat,exp(i-10))
+		wMat[i,:]=ws.T
+	return wMat
+
+def ridgeMain():
+	abX,abY=loadDataSet('abalone.txt')
+	ridgeWeights=ridgeTest(abX,abY)
+	print ridgeWeights
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(ridgeWeights)
+	plt.show()
+
+############8.4  Forward stagewise linear regression
+def regularize(xArr):
+	xMat = mat(xArr)
+	xMeans = mean(xMat,0) # 均值，对每一列求均值
+	xVar = var(xMat,0) # 方差
+	xMat = (xMat - xMeans)/xVar # 随机变量标准化
+	return xMat
+
+def stageWise(xArr,yArr,eps=0.01,numIt=100):
+	xMat = mat(xArr); yMat=mat(yArr).T
+	yMean = mean(yMat,0)
+	yMat = yMat - yMean
+	xMat = regularize(xMat)
+	m,n=shape(xMat)
+	ws = zeros((n,1)); wsTest = ws.copy(); wsMax = ws.copy()
+	returnMat = zeros((numIt,n))
+	for i in range(numIt):
+		#print ws.T
+		lowestError = inf;
+		for j in range(n):
+			for sign in [-1,1]:
+				wsTest = ws.copy()
+				wsTest[j] += eps*sign
+				yTest = xMat*wsTest
+				rssE = rssError(yMat.A,yTest.A)
+				if rssE < lowestError:
+					lowestError = rssE
+					wsMax = wsTest
+		ws = wsMax.copy()
+		returnMat[i,:]=ws.T
+	return returnMat
+
+def stageMain():
+	xArr,yArr=loadDataSet('abalone.txt')
+	res = stageWise(xArr,yArr,0.001,5000)
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(res)
+	plt.show()
+
+########################### LEGO
+
 
 if __name__ == "__main__":
 	# main1()
-	lwlrMain()
+	# lwlrMain()
+	# ridgeMain()
+	# stageMain()
+	legoMain()
