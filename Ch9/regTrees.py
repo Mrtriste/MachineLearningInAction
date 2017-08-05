748afc344db913574ded0a98f4d37b0b3d723b13
MrTriste <jinchengwone@gmail.com>
2017-08-06 00:16:03 +0800

    update regTrees.py

--- Ch9/regTrees.py
+++ Ch9/regTrees.py
@@ -111,6 +111,7 @@ def getMean(tree):
 	if isTree(tree['left']): tree['left'] = getMean(tree['left'])
 	return (tree['left']+tree['right'])/2.0
 
+#简单的剪枝，如果merge后的误差更小就merge
 def prune(tree, testData):
 	if shape(testData)[0] == 0: 
 		return getMean(tree)
@@ -133,6 +134,95 @@ def prune(tree, testData):
 			return tree
 	else: return tree
 
+def pruneMain():
+	myDat2= loadDataSet('ex2.txt')
+	myMat2=mat(myDat2)
+	myTree=createTree(myMat2, ops=(0,1))
+	myDatTest=loadDataSet('ex2test.txt')
+	myMat2Test=mat(myDatTest)
+	print prune(myTree, myMat2Test)
+
+################################################### model tree
+def linearSolve(dataSet):
+	m,n = shape(dataSet)
+	X = mat(ones((m,n))); Y = mat(ones((m,1)))
+	X[:,1:n] = dataSet[:,0:n-1]; Y = dataSet[:,-1]
+	xTx = X.T*X
+	if linalg.det(xTx) == 0.0:
+		raise NameError('This matrix is singular, cannot do inverse,\n\
+				try increasing the second value of ops')
+	ws = xTx.I * (X.T * Y)
+	return ws,X,Y
+
+def modelLeaf(dataSet):
+	ws,X,Y = linearSolve(dataSet)
+	return ws
+
+def modelErr(dataSet):
+	ws,X,Y = linearSolve(dataSet)
+	yHat = X * ws
+	return sum(power(Y - yHat, 2))
+
+def modelMain():
+	myDat2= mat(loadDataSet('exp2.txt'))
+	myMat2=mat(myDat2)
+	print createTree(myMat2, modelLeaf, modelErr,(1,10))
+
+############################################## Test Set
+def regTreeEval(model, inDat):
+	# 返回回归树叶节点上的值
+	return float(model)
+
+def modelTreeEval(model, inDat):
+	n = shape(inDat)[1]
+	X = mat(ones((1,n+1)))
+	X[:,1:n+1]=inDat
+	return float(X*model)
+
+#对测试集中每一个样例点做一次treeForeCast
+def treeForeCast(tree, inData, modelEval=regTreeEval):
+	if not isTree(tree): return modelEval(tree, inData)
+	# 分裂特征的值大于分裂值就到左节点
+	if inData[tree['spInd']] > tree['spVal']:
+		if isTree(tree['left']):
+			return treeForeCast(tree['left'], inData , modelEval)
+		else:
+			return modelEval(tree['left'], inData)
+	else:
+		if isTree(tree['right']):
+			return treeForeCast(tree['right'], inData , modelEval)
+		else:
+			return modelEval(tree['right'], inData)
+
+def createForeCast(tree, testData, modelEval=regTreeEval):
+	m=len(testData)
+	yHat = zeros((m,1))
+	for i in range(m):
+		yHat[i,0]=treeForeCast(tree, mat(testData[i]), modelEval)
+	return yHat
+
+def ForeCastMain():
+	trainMat=mat(loadDataSet('bikeSpeedVsIq_train.txt'))
+	testMat=mat(loadDataSet('bikeSpeedVsIq_test.txt'))
+	### regression tree
+	myTree=createTree(trainMat, ops=(1,20))
+	yHat = createForeCast(myTree, testMat[:,0])
+	print corrcoef(yHat, testMat[:,1],rowvar=0)[0,1]
+	### model tree
+	myTree=createTree(trainMat, modelLeaf,modelErr,(1,20))
+	yHat = createForeCast(myTree, testMat[:,0],
+	modelTreeEval)
+	print corrcoef(yHat, testMat[:,1],rowvar=0)[0,1]
+	### linear regression
+	ws,X,Y=linearSolve(trainMat)
+	for i in range(shape(testMat)[0]):
+		yHat[i]=testMat[i,0]*ws[1,0]+ws[0,0]
+	print corrcoef(yHat, testMat[:,1],rowvar=0)[0,1]
+
+
 if __name__ == "__main__":
 	# main1()
-	regMain()
+	# regMain()
+	# pruneMain()
+	# modelMain()
+	ForeCastMain()
